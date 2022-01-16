16168072f15fd7b73fcce38e3009b33a3bf8dd5e
Piotr Bogdański <pbogdansk@student.agh.edu.pl>
2022-01-16 17:06:21 +0100

    dodanie menu opcji wyboru piętra na którym znajdzie się nowy pokój oraz wprowadzanie ilości osób do pokoju

--- menu_admin.cpp
+++ menu_admin.cpp
@@ -3,6 +3,9 @@
 int roomQualityMenu, numberOfPeopleMenu, floorMenu, surfaceAreaMenu;
 void howManyPeople();
 void menu_addRoom();
+void whatSurfaceArea();
+int whatFloor();
+
 void menu_admin(Admin account) {
 
 	//test
@@ -42,6 +45,7 @@ void menu_admin(Admin account) {
 	case 0:
 		roomQualityMenu = 3;
 		howManyPeople();
+		floorMenu = whatFloor();
 		break;
 	case 1:
 		roomQualityMenu = 2;
@@ -60,3 +64,72 @@ void howManyPeople() {
 	std::cout << "ilu ludzi bêdzie w nowym pokoju?\n";
 	std::cin >> numberOfPeopleMenu;
 }
+int whatFloor() {
+	menu_gui::reset();
+	menu_gui::add_top_text("Wybierz pietro pokoju do dodania:");
+	
+	menu_gui::add_option("1");	
+	menu_gui::add_option("2");	
+	menu_gui::add_option("3");	
+	menu_gui::add_option("4");	
+	menu_gui::add_option("5");	
+	menu_gui::add_option("6");
+	menu_gui::add_option("7");
+	menu_gui::add_option("8");
+	menu_gui::add_option("9");
+	menu_gui::add_option("10");
+	menu_gui::add_option("11");
+	menu_gui::add_option("12");
+
+	switch (menu_gui::display())
+	{
+
+	case 0:
+		return 1;
+		break;
+	case 1:
+		return 2;
+		break;
+	case 2:
+		return 3;
+		break;
+	case 3:
+		return 4;
+		break;
+	case 4:
+		return 5;
+		break;
+	case 5:
+		return 6;
+		break;
+	case 6:
+		return 7;
+		break;
+
+	case 7:
+		return 8;
+		break;
+
+	case 8:
+		return 9;
+		break;
+
+	case 9:
+		return 10;
+		break;
+
+	case 10:
+		return 11;
+		break;
+
+	case 11:
+		return 12;
+		break;
+
+	}
+}
+void whatSurfaceArea() {
+	std::cout << "Jaka bedzie powierzchnia nowego pokoju?\n";
+	std::cin >> surfaceAreaMenu;
+}
+

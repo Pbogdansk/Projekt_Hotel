1d4f2c1eb8e6c750bab887f6b7237778e579761f
ho-ody <r4f4l135@gmail.com>
2022-01-16 01:59:13 +0100

    TDD Red, findEmailAdressInGivenArray, dodano testy w przypadku nie znalezienia adersu email

--- Tests_Projekt_Hotel/testLogInAccount.cpp
+++ Tests_Projekt_Hotel/testLogInAccount.cpp
@@ -31,5 +31,27 @@ public:
 		int indexInArrayOfGivenEmail = findEmailAdressInGivenArray(accounts, 4, wantedEmail);
 		Assert::AreEqual(indexInArrayOfGivenEmail, 3);
 	}
+	TEST_METHOD(CannotFindGivenAdress0)
+	{
+		Person accounts[2];
+		accounts[0] = Person("Bazyli", "Kucharski", 13021954, "BazyliKucharski@dayrep.com");
+		accounts[1] = Person("Tekla", "Czarnecka", 30041949, "TeklaCzarnecka@teleworm.us");
+		string wantedEmail = "GertrudaDudek@jourrapide.com";
+
+		int indexInArrayOfGivenEmail = findEmailAdressInGivenArray(accounts, 2, wantedEmail);
+		Assert::AreEqual(indexInArrayOfGivenEmail, -1);
+	}
+	TEST_METHOD(CannotFindGivenAdress1)
+	{
+		Person accounts[4];
+		accounts[0] = Person("Bazyli", "Kucharski", 13021954, "BazyliKucharski@dayrep.com");
+		accounts[1] = Person("Tekla", "Czarnecka", 30041949, "TeklaCzarnecka@teleworm.us");
+		accounts[2] = Person("Dominik ", "Olszewski", 20071963, "DominikOlszewski@jourrapide.com");
+		accounts[3] = Person("Klementyna", "Zawadzka", 16061960, "KlementynaZawadzka@dayrep.com");
+		string wantedEmail = "KlaraWalczak@dayrep.com";
+
+		int indexInArrayOfGivenEmail = findEmailAdressInGivenArray(accounts, 4, wantedEmail);
+		Assert::AreEqual(indexInArrayOfGivenEmail, -1);
+	}
 	};
 }

fefcd27b7609c4f825f6d66820685f3a268acef2
Bartek Banaś <bartek.banas675@gmail.com>
2022-01-18 23:06:19 +0100

    createAccount, ale upośledzone
    Bo liczba kont jest ograniczona

--- createAccount.cpp
+++ createAccount.cpp
@@ -5,18 +5,26 @@ void createAccount(Customer customersAccounts[]) {
 	string newSurname;
 	int newDateOfBirth;
 	string newEmail;
-	string newPassword;
+	string newPassword, newPassword2;
 	system("cls");		// czyszczenie ekranu (wed³ug Rafa³a przynajmniej)
 	cout << "Prosze podac panskie imie: ";
 	cin >> newName;
 	cout << endl << "Prosze podac panskie nazwisko: ";
 	cin >> newSurname;
-	cout << endl << "Prosze podac panska date urodzenia: ";
+	cout << endl << "Prosze podac panska date urodzenia w formacie (ddnnrrrr): ";
 	cin >> newDateOfBirth;
 	cout << endl << "Prosze podac panski adres e-mail: ";
 	cin >> newEmail;
 	cout << endl << "Prosze podac panskie haslo ";
 	cin >> newPassword;
-
-	customersAccounts[11] = Customer(newName, newSurname, newDateOfBirth, newEmail, newPassword);
+	cout << "Porsze powtorzyc haslo: ";
+	cin >> newPassword2;
+	
+	if (newPassword == newPassword2) {
+		customersAccounts[10] = Customer(newName, newSurname, newDateOfBirth, newEmail, newPassword);
+		cout << "Dziekujemy za stworzenie konta w naszym hotelu" << endl;
+	}
+	else {
+		cout << "Podane hasla nie pokrywaja sie, prosze powtorzyc probe tworzenia konta" << endl;
+	}
 }
